47570ed06f143fa8afcad28562b40c0a0736e472
layc41 <64671431+layc41@users.noreply.github.com>
2020-10-06 00:32:23 -0500

    added modal conditional functionality

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-function Modal({currentPhoto}) {
+function Modal({ onClose, currentPhoto}) {
 
+    
     const {name, category, description, index} = currentPhoto;
 
     return (
@@ -10,7 +11,7 @@ function Modal({currentPhoto}) {
                 <h3 className="modalTitle">Photo Name</h3>
                 <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
                 <p>{description}</p>
-                <button type="button">
+                <button onClick={onClose} type="button">
                     Close this modal
                 </button>
             </div>
